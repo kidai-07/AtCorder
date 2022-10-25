159fcea4655ade6008dc7480ab870a1095cd4ec8
DragoPuni <drarrylove10@gmail.com>
2022-10-26 00:21:57 +0900

    [update]

---
+++ .vscode/果物屋さんでお買い物.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int N, S;
+  cin >> N >> S;
+  vector<int> A(N), P(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+  for (int i = 0; i < N; i++) {
+    cin >> P.at(i);
+  }
+ 
+  // リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
+  // ここにプログラムを追記
+
+S - A.at(1) == B.at(i) ---こんな感じで多分ループさせる
+
+}
