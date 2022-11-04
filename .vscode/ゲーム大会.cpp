161ce8ff2d460557fd644846cdb1e5702493b0b9
kidai-07 <drarrylove10+1@gmail.com>
2022-11-04 23:56:54 +0900

    [update]

--- .vscode/ゲーム大会.cpp
+++ .vscode/ゲーム大会.cpp
@@ -1,38 +1,24 @@
-//入力形式
-// N人が参加してM試合行われた
-// Aが勝利Bが敗北
-// N M
-// A B
-// A B(これがM行続く)
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int N, M;
-    cin >> N >> M;
-    vector<int> A(M), B(M);
-    for (int i = 0; i < M; i++)
-    {
-        cin >> A.at(i) >> B.at(i);
-    }
-    vector<string> sheet = {
-        "〇",
-        "〇",
-        "×",
-        "-",
-    }; // (ここで"試合結果の表"の2次元配列を宣言)
-    for (int i = 0; i < (M - 1) * (M - 1); i++)
-    {
-        cout << sheet.at(i);
-        if (i = (M - 1))
-        {
-            cout << endl; // 末尾なら改行
-        }
-        else
-        {
-            cout << " "; // それ以外なら空白
+    vector<vector<int>> data(3, vector<int>(4));
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 4; j++) {
+            cin >> data.at(i).at(j);
         }
     }
+
+    int count = 0;
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 4; j++){
+            if (data.at(i).at(j) == 0){
+                count++; 
+            }
+        }
+    }
+
+    cout << count << endl;
 }
