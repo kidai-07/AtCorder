7e705d67ce45683c00ae43055e8baf7db49a88b6
kidai-07 <drarrylove10+1@gmail.com>
2022-11-13 14:17:31 +0900

    update

--- .vscode/python/test.py
+++ .vscode/python/test.py
@@ -1,2 +1,22 @@
-def test():
-    print("Fabor")
+# データ型の復習
+a = 7
+print(a)
+
+b = a-3
+
+print(b)
+
+# 型の変換
+print(int(True))
+
+# リスト
+list = [a,b]
+print(list)
+print(list[0])
+
+# 条件分岐
+disaster = True
+if disaster:
+        print("don")
+else:
+        print("dondon")
