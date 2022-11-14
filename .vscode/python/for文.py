a787d90911926c597610797bc5fe5b6622e7e647
kidai-07 <drarrylove10+1@gmail.com>
2022-11-14 21:53:28 +0900

    test

---
+++ .vscode/python/for文.py
@@ -0,0 +1,16 @@
+# forによる反復処理
+
+rabbits = ['flo', 'Mos', 'cot','Pet']
+for rabbit in rabbits:
+	print(rabbit)
+
+# タプルやリストをforで処理すると、一度に1つずつ要素が取り出される。
+
+word = 'cat'
+for i in word:
+    print(i)
+    
+# とても簡単な問題をpythonで解こう
+D = int(input())
+y = D/100
+print(y)
